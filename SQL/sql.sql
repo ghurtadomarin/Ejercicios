0a6f081fd658f07df8208e15c6684b47a5b80b15
ghurtadomarin <ghurtadomarin@cifpfbmoll.eu>
2022-12-13 17:08:20 +0100

    examen

--- SQL/sql.sql
+++ SQL/sql.sql
@@ -1,17 +1,33 @@
+--mariadb
 create table customer (
     fname varchar (20) not null,
     mname varchar (20) not null,
     lname varchar (20) not null,
     mail varchar (100) not null,
-    date_of_birth date, 
-    primary key (mail));
+    address varchar (200) unique,
+    date_of_birth date '1900-1-1', 
+    constraint pk_costumer primary key (mail));
 
-
-create table order(
-    date_time date,
-    order_number int,
+create table order (
+    date_time date timestamp,
+    order_number int auto_increment ,
     mail varchar(100),
     primary key(order_number, mail),
-    foreign key(mail)
-    references customer(mail)
+    constraint fk_order foreign key(mail) references customer(mail)
 );
+
+create table product (
+
+)
+
+
+comandos mariadb{
+    drop database/table"nombre basae de datos"
+    use  base_de_datos   --para entrar en la base de datos
+    insert into tabla ()
+    select * from tabla
+    descri
+    insert into tabla (Atributo) value ('algo')
+}
+
+--PostgresSQL
